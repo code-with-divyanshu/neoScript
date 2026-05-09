db5d94872894b18326d4884d72a3deba8f8980e7
code-with-divyanshu <divyanshubartwal67@gmail.com>
2026-05-09 23:41:16 +0530

    add update user profile functionality with mutation hook

--- frontend/src/lib/api/user.api.ts
+++ frontend/src/lib/api/user.api.ts
@@ -3,3 +3,11 @@ import { api } from "../axios";
 export const getUserDetails = async () => {
   return api.get("/user/me");
 };
+
+export const updateUserProfile = async (payload: any) => {
+  return api.put("/user/update-profile", payload, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
--- frontend/src/lib/hooks/api/useUser.ts
+++ frontend/src/lib/hooks/api/useUser.ts
@@ -1,5 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
-import { getUserDetails } from "../../api/user.api";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getUserDetails, updateUserProfile } from "../../api/user.api";
+import { useStore } from "../../store";
+import { toast } from "react-hot-toast";
 
 export const useGetUserProfile = () => {
   return useQuery({
@@ -7,6 +9,26 @@ export const useGetUserProfile = () => {
     queryFn: async () => {
       const res = await getUserDetails();
       return res.data.user;
+    },
+  });
+};
+
+export const useUpdateUserProfile = () => {
+  const queryClient = useQueryClient();
+  const { login, token } = useStore((state) => state);
+
+  return useMutation({
+    mutationFn: updateUserProfile,
+    onSuccess: (response: any) => {
+      const updatedUser = response?.data?.user;
+      toast.success("Profile updated successfully");
+      if (updatedUser) {
+        login(updatedUser, token ?? "");
+        queryClient.invalidateQueries({ queryKey: ["user-profile"] });
+      }
+    },
+    onError: (error: any) => {
+      toast.error(error?.response?.data?.message || "Profile update failed.");
     },
   });
 };
